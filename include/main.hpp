ad2bd9171b3fbb514c320c68ec0a0c320e824247
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2022-11-23 17:13:03 -0300

    include: add main.hpp

---
+++ include/main.hpp
@@ -0,0 +1,28 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of texgen.
+ *
+ *   texgen is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   texgen is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with texgen.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TEXGEN_MAIN_HPP
+#define TEXGEN_MAIN_HPP
+
+#include <vector>
+
+typedef std::vector<double> Vec;
+
+#endif
