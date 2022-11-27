d8bb002b8b7613e1466c13d8d871266fcc6ad523
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2022-11-27 20:57:28 -0300

    solver: add SLP
    Experimental, based on textopt's. Currently does not work.

---
+++ include/solver/slp.hpp
@@ -0,0 +1,96 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of texgen.
+ *
+ *   texgen is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   texgen is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with texgen.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SOLVER_SLP_HPP
+#define SOLVER_SLP_HPP
+
+#include "main.hpp"
+#include <cmath>
+#include <cstddef>
+
+class SLP{
+    public:
+    /**
+     * Update `x` based on derivatives and constraints.
+     */
+    void update(Vec& x, const Vec& dfdx, const Vec& g, const Vec& dgdx, const double xmin, const double xmax) const;
+
+    private:
+    const double EPS = 1e-7;
+    /**
+     * Dot product between two vectors.
+     */
+    inline Vec dot(const Vec& df, const Vec& dg, const size_t N,  const size_t M) const{
+        Vec c(M, 0);
+        for(size_t i = 0; i < N; ++i){
+            for(size_t j = 0; j < M; ++j){
+                c[j] += df[i]*dg[i*M + j];
+            }
+        }
+        return c;
+    }
+    /**
+     * Multiplies a matrix and a vector.
+     */
+    inline Vec mat_dot_vec(const Vec& m, const Vec& v) const{
+        const size_t l = m.size()/v.size();
+        const size_t n = v.size();
+        Vec r(l,0);
+        for(size_t i = 0; i < l; ++i){
+            for(size_t j = 0; j < n; ++j){
+                r[i] += m[i*n + j]*v[j];
+            }
+        }
+        return r;
+    }
+    /**
+     * Scales a vector or matrix.
+     */
+    inline Vec scale(const double s, const Vec& v) const{
+        auto sv = v;
+        for(size_t i = 0; i < v.size(); ++i){
+            sv[i] *= s;
+        }
+        return sv;
+    }
+    /**
+     * Normalizes a vector.
+     */
+    inline Vec normalized(const Vec& v, const size_t N, const size_t M) const{
+        Vec norm(M, 0);
+        for(size_t i = 0; i < N; ++i){
+            for(size_t j = 0; j < M; ++j){
+                norm[j] += v[i*M + j]*v[i*M + j];
+            }
+        }
+        for(auto& n : norm){
+            n = std::sqrt(n);
+        }
+        auto svn = v;
+        for(size_t i = 0; i < N; ++i){
+            for(size_t j = 0; j < M; ++j){
+                svn[i*M + j] /= norm[j];
+            }
+        }
+        return svn;
+    }
+};
+
+#endif
---
+++ src/solver/slp.cpp
@@ -0,0 +1,63 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of texgen.
+ *
+ *   texgen is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   texgen is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with texgen.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "solver/MMASolver.hpp"
+#include "solver/slp.hpp"
+#include <iostream>
+
+void SLP::update(Vec& x, const Vec& dfdx, const Vec& g, const Vec& dgdx, const double xmin, const double xmax) const{
+    // x_{i+1} = x_i + l_i*dfdx_i
+    // dgdx_i*(x_{i+1}-x_i) <= g_i
+    //
+    // dgdx_i*(l_i*dfdx_i) <= g_i
+    // l_i <= g_i/(dgdx_i*dfdx_i)
+    const size_t N = x.size();
+    const size_t M = g.size();
+    
+    // Normalize vectors (currently assuming single constraint)
+    const auto norm_dfdx = this->normalized(dfdx, N, 1);
+    const auto norm_dgdx = this->normalized(dgdx, N, M);
+
+    MMASolver mma(1, 0, 0, 1e3, 1);
+    mma.SetAsymptotes(0.5, 0.7, 1.2);
+
+    // Calculate l_i
+    const Vec dg_df = this->dot(norm_dfdx, norm_dgdx, N, M);
+    double l_i = 0;//-g[0]/dg_df;
+    for(size_t i = 0; i < M; ++i){
+        l_i += -g[i]/dg_df[i];
+    }
+    l_i /= M;
+
+    // Make sure l_i does not make x too low or too high.
+    for(size_t i = 0; i < N; ++i){
+        if(x[i] + l_i*norm_dfdx[i] < xmin){
+            l_i = 0.5*(xmin-x[i])/norm_dfdx[i];
+        } else if(x[i] + l_i*norm_dfdx[i] > xmax){
+            l_i = 0.5*(xmax-x[i])/norm_dfdx[i];
+        }
+    }
+
+    // Update x
+    for(size_t i = 0; i < N; ++i){
+        x[i] += l_i*norm_dfdx[i];
+    }
+
+}
